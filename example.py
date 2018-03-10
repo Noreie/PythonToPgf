533c2bfa4d258319de9f65627fc88b39a1182ab3
Noreie <24235505+Noreie@users.noreply.github.com>
2018-03-10 13:41:35 +0100

    Add files via upload

---
+++ example.py
@@ -0,0 +1,27 @@
+import pytopgf as p2p
+import numpy as np
+import math
+
+def ema(y, a):
+    s = []
+    s.append(y[0])
+    for t in range(1, len(y)):
+        s.append(a * y[t] + (1-a) * s[t-1])
+    return np.array(s)
+
+p2p.sexyplot() #Kan slås av
+fig, ax  = p2p.newfig(345,ratio=1.5)
+
+
+
+y = [0]*200
+y.extend([20]*(1000-len(y)))
+s = ema(y, 0.01)
+
+
+
+ax.plot(s)
+ax.set_xlabel('X Label')
+ax.set_ylabel('EMA')
+fig.show()
+#p2p.savefig("ema")
---
+++ pytopgf.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+import numpy as np
+import matplotlib as mpl
+
+def plotstyle(style=None)
+    """Plotstyle
+    Style: 'Draft' (Default None)
+    """
+    
+pgf_with_latex = {                      # setup matplotlib to use latex for output
+"pgf.texsystem": "pdflatex",        # change this if using xetex or lautex
+"text.usetex": True,                # use LaTeX to write all text
+"font.family": [0],
+"font.serif": [],                   # blank entries should cause plots to inherit fonts from the document
+"font.sans-serif": [],
+"font.monospace": [],
+"axes.labelsize": 8,               # LaTeX default is 10pt font.
+"font.size": 8,
+"legend.fontsize": 8,               # Make the legend/label fonts a little smaller
+"xtick.labelsize": 8,
+"ytick.labelsize": 8,
+#"figure.figsize": figsize(0.9),     # default fig size of 0.9 textwidth
+"pgf.preamble": [
+r"\usepackage[utf8x]{inputenc}",    # use utf8 fonts becasue your computer can handle it :)
+r"\usepackage[T1]{fontenc}",        # plots will be generated using this preamble
+        ]
+    }
+mpl.rcParams.update(pgf_with_latex)
+import matplotlib.pyplot as plt
+
+def figsize(fig_width_pt, scale=None, ratio=None): # Used through newfig()
+    """Define the figure size. 
+    fig_width_pt: Get this from LaTeX using \\the\\textwidth
+    Scale: Optional (Default: 0.9)
+    Ratio: Optional (height/width. Default:Golden ratio)
+    """
+    inches_per_pt = 1.0/72.27                       # Convert pt to inch
+    if ratio == None:
+        ratio= (np.sqrt(5.0)-1.0)/2.0            # Aesthetic ratio (you could change this), default Golden ratio
+    if scale==None:
+        scale=0.9
+    fig_width = fig_width_pt*inches_per_pt*scale    # width in inches
+    fig_height = fig_width*ratio              # height in inches
+    fig_size = [fig_width,fig_height]
+    return fig_size
+
+
+def newfig(width,scale=None,ratio=None):
+    """Creates the figure with the added subplot. This functions calles figsize()
+    width: Get this from LaTeX using \\the\\textwidth
+    Scale: Optional (Default: 0.9)
+    Ratio: Optional (height/width. Default:Golden ratio)
+    """
+    plt.clf()
+    fig = plt.figure(figsize=figsize(width,scale,ratio))
+    ax = fig.add_subplot(111)
+    return fig, ax
+
+def savefig(filename):
+    """Saves the file
+    filename: String"""
+    plt.savefig('{}.pgf'.format(filename), bbox_inches='tight')
+    plt.savefig('{}.pdf'.format(filename), bbox_inches='tight')
+
+def sexyplot():
+    mpl.rcParams['lines.linewidth'] = 0.8
+    mpl.rcParams['axes.prop_cycle']=mpl.cycler(color=['k','k'])
+    mpl.rcParams['axes.grid']=True
+    mpl.rcParams['legend.loc'] = 'Best'
+    #mpl.rcParams['grid.color'] ='k'
+    mpl.rcParams['grid.linestyle']='--'
